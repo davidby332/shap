e060db294e5757147d7857d589ed1c2004638b88
davidby332 <davidby332@gmail.com>
2022-02-11 13:18:17 -0600

    fix dependence plot colorbar size

--- shap/plots/_scatter.py
+++ shap/plots/_scatter.py
@@ -355,7 +355,7 @@ def scatter(shap_values, color="#1E88E5", hist=True, axis_color="#333333", cmap=
         cb.set_alpha(1)
         cb.outline.set_visible(False)
         bbox = cb.ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-        cb.ax.set_aspect((bbox.height - 0.7) * 20)
+        # cb.ax.set_aspect((bbox.height - 0.7) * 20)
 
     # handles any setting of xmax and xmin
     # note that we handle None,float, or "percentile(float)" formats
